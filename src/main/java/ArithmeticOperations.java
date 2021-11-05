349e48ae5e4f95a8db945ff0e713b8d8595fa638
Benzin82 <tema_star@mail.ru>
2021-11-05 22:12:24 +0300

    second program "ArithmeticOperation"

---
+++ src/main/java/ArithmeticOperations.java
@@ -0,0 +1,20 @@
+public class ArithmeticOperations<result> {
+
+    public static void main(String[] args){
+    int a = 15;
+    int b = 5;
+    int result;
+
+    result = a + b;
+    System.out.println ("Сумма аргуметов равна " + result);
+
+    result = a - b;
+    System.out.println ("Разность аргуметов равна " + result);
+
+    result = a * b;
+    System.out.println ("Множество аргуметов равна " + result);
+
+    result = a / b;
+    System.out.println ("Частность аргуметов равна " + result);}
+    }
+
