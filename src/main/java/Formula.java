1422cde26ecc2a4da0406497750a3f32fa8859fa
Benzin82 <tema_star@mail.ru>
2021-11-05 23:36:23 +0300

    program "Formula"

---
+++ src/main/java/Formula.java
@@ -0,0 +1,15 @@
+public class Formula
+{
+     public static void main(String[] args) {
+          // Площадь прямоугольника равна произведению сторон a and b
+          int a;
+          int b;
+          int s;
+          a = 9;
+          b = 6;
+          s = a*b;
+          System.out.println("Площадь прямоугольника равна " + s);
+     }
+
+
+}
