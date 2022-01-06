062d721b37ea9f9eae225f509809470c4c48f493
Benzin82 <tema_star@mail.ru>
2022-01-06 20:53:09 +0300

    program "Calculate"

--- src/main/java/Lession2/Arith.java
+++ src/main/java/Lession2/Arith.java
@@ -2,29 +2,36 @@ package Lession2;
 
 public class Arith {
 
+    public static void main(String[] args) {
 
+        long sum = summa(5, 3);
+        System.out.println("Сумма равна = " + sum);
 
-    public static int summa (int arg1, int arg2) {
-        int sum = summa(5, 5);
-        System.out.println(sum);
+        long dif = difference(5, 5);
+        System.out.println("Разность равна = " + dif);
+
+        long mul = multiply(5, 5);
+        System.out.println("Множество равно = " + mul);
+
+        long div = divide(5, 5);
+        System.out.println("Результат деления равен = " + div);
+}
+    private static int summa (int arg1, int arg2) {
         return arg1 + arg2;
     }
-
-        public static int difference(int arg1, int arg2) {
-            int dif = difference(5, 5);
-            return arg1 - arg2;
+       public static int difference(int arg1, int arg2) {
+                        return arg1 - arg2;
 
         }
-
             public static int multiply (int arg1, int arg2) {
-                int mult = multiply (5, 5);
-                return arg1 * arg2;
+                                return arg1 * arg2;
             }
 
                 public static int divide (int arg1, int arg2) {
-                    int div = divide(5, 5);
-                    return arg1 / arg2;
+                                        return arg1 / arg2;
                 }
 
+
     }
+
 
