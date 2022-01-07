e5a6f830a0607cf5be78df0acde7429adac433a9
Benzin82 <tema_star@mail.ru>
2022-01-07 12:00:40 +0300

    program "Fibonacci"

---
+++ src/main/java/HW6/TestArr.java
@@ -0,0 +1,21 @@
+
+
+package HW6;
+
+import java.util.Scanner;
+
+public class TestArr {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println("Введите длину массива = " + n);
+
+        public static int getRandomDiceNumber () {
+        for (int i = 0; i <= n; n++) {
+            ;
+        }
+
+    }
+}
